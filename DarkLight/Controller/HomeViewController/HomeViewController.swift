a9202d7f76deef140c0dda775cb5d3267d86cc83
Thanh Cong <sythanhcong.dev@gmail.com>
2023-05-02 09:49:14 +0700

    start project not use Main Storyboard

---
+++ DarkLight/Controller/HomeViewController/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  DarkLight
+//
+//  Created by Thanh Cong on 01/05/2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
